d7691c26e87972c148812e3fb4b49c53f3e42417
Your Name <zhuoxinyun1123@gmail.com>
2023-08-11 20:15:33 +0100

    sql

--- card_class.py
+++ card_class.py
@@ -1,9 +1,26 @@
-from project_knowledge import get_quote
+## from project_knowledge import get_quote
+
+
 class Card:
-    def __init__(self, name, attributes, quote=None):
+    def __init__(self, name, attribute_names, attribute_scores, category, quote=None):
         self.name = name
-        self.attributes = attributes
+        self.attribute_names = attribute_names
+        self.attribute_scores = attribute_scores
+        self.category = category
         self.quote = quote
 
-    def fetch_quote(self):
-        self.quote = get_quote(self.name)
+##    def fetch_quote(self):
+##        self.quote = get_quote(self.name)
+
+
+''' 
+Example usage
+attributes = ['Singing Voice', 'Creativity', 'Composing']
+card_attribute_scores = {
+    'Singing Voice': 8,
+    'Creativity': 5,
+    'Composing': 4
+}
+
+sample_card = Card("Beyonce", card_attributes, card_attribute_scores, 'Artist', None)
+'''
