38fbbd5cbd53032f1b4147db0cf05cba3463940d
Your Name <zhuoxinyun1123@gmail.com>
2023-08-11 20:04:06 +0100

    read cards from database

---
+++ read_cards_from_database.py
@@ -0,0 +1,74 @@
+import mysql.connector
+
+from card_class import Card
+
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    user='root',
+    password='Zxy005676!',
+    database='top_trumps'
+)
+cursor = connection.cursor()
+
+
+# Function to fetch cards from the database
+def fetch_cards():
+    cursor.execute('SELECT Cards.name, Categories.name, Attributes.name, Card_Attribute_Score.score FROM Cards \
+                    JOIN Categories ON Cards.category_id = Categories.id \
+                    JOIN Card_Attribute_Score ON Cards.id = Card_Attribute_Score.card_id \
+                    JOIN Attributes ON Card_Attribute_Score.attribute_id = Attributes.id')
+
+    cards = {}
+    for row in cursor.fetchall():
+        card_name, category, attribute_name, score = row
+        if card_name not in cards:
+            cards[card_name] = {
+                'category': category,
+                'attributes': {},
+            }
+        cards[card_name]['attributes'][attribute_name] = score
+
+    return cards
+
+
+# Fetch cards from the database
+cards_data = fetch_cards()
+
+# Transform data into Card objects
+cards = []
+for card_name, card_data in cards_data.items():
+    category = card_data['category']
+    attribute_names = list(card_data['attributes'].keys())
+    attribute_scores = card_data['attributes']
+
+    card = Card(card_name, attribute_names, attribute_scores, category)
+    cards.append(card)
+
+# Close the database connection
+connection.close()
+
+'''
+# Print the fetched cards
+for card in cards:
+    print("Card:", card.name)
+    print("Category:", card.category)
+    print("Attribute Names:", card.attribute_names)
+    print("Attribute Scores:", card.attribute_scores)
+    print()
+
+output:
+Card: Beyonce
+Category: Artist
+Attribute Names: ['Singing Voice', 'Creativity', 'Painting', 'Composing']
+Attribute Scores: {'Singing Voice': 10, 'Creativity': 7, 'Painting': 0, 'Composing': 5}
+
+Card: Sinead O'Connor
+Category: Artist
+Attribute Names: ['Singing Voice', 'Creativity', 'Painting', 'Composing']
+Attribute Scores: {'Singing Voice': 9, 'Creativity': 8, 'Painting': 0, 'Composing': 8}
+
+Card: Ruth Bader Ginsburg
+Category: Academic
+Attribute Names: ['Intelligence', 'Political Influence', 'Determination', 'Rebellious']
+Attribute Scores: {'Intelligence': 9, 'Political Influence': 9, 'Determination': 6, 'Rebellious': 9}
+'''
