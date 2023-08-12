3c0833f0d0d119107a3c6a56c51e63401d6213e1
Emma Holden <98665539+EmmaHolden@users.noreply.github.com>
2023-08-12 20:11:43 +0100

    Revert "sql"

--- card_class.py
+++ card_class.py
@@ -1,26 +1,9 @@
-## from project_knowledge import get_quote
-
-
+from project_knowledge import get_quote
 class Card:
-    def __init__(self, name, attribute_names, attribute_scores, category, quote=None):
+    def __init__(self, name, attributes, quote=None):
         self.name = name
-        self.attribute_names = attribute_names
-        self.attribute_scores = attribute_scores
-        self.category = category
+        self.attributes = attributes
         self.quote = quote
 
-##    def fetch_quote(self):
-##        self.quote = get_quote(self.name)
-
-
-''' 
-Example usage
-attributes = ['Singing Voice', 'Creativity', 'Composing']
-card_attribute_scores = {
-    'Singing Voice': 8,
-    'Creativity': 5,
-    'Composing': 4
-}
-
-sample_card = Card("Beyonce", card_attributes, card_attribute_scores, 'Artist', None)
-'''
+    def fetch_quote(self):
+        self.quote = get_quote(self.name)
