ac6db1d7ddbee326f952014d639c4e18ae19bc0b
Emma Holden <emmaholden01204@gmail.com>
2023-08-12 10:53:01 +0100

    actor category added

--- player_deck.py
+++ player_deck.py
@@ -5,13 +5,21 @@ class PlayerDeck:
         self.categories = {
             "Artist": [
                 Card("Beyonce", "beyonce.png", {'Singing Voice': 10, 'Creativity': 7, 'Painting': 0, 'Composing': 5}),
-                Card("Sinead O'Connor", "placeholder.png", {'Singing Voice': 9, 'Creativity': 8, 'Painting': 0, 'Composing': 8})
+                Card("Sinead O'Connor", "placeholder.png", {'Singing Voice': 9, 'Creativity': 8, 'Painting': 0, 'Composing': 8}),
+                Card("Frida Kahlo", "frida.png", {'Singing Voice': 2, 'Creativity': 10, 'Painting': 10, 'Composing': 2}),
+                Card("Taylor Swift", "placeholder.png", {'Singing Voice': 8, 'Creativity': 10, 'Painting': 2, 'Composing': 9}),
+            ],
+            "Actors": [
+                Card("Penelope Cruz", "placeholder.png", {'Acting': 8, 'Comedy': 7, 'Charisma': 9, 'Confidence': 9}),
+                Card("Susan Sarandon", "placeholder.png", {'Acting': 9, 'Comedy': 8, 'Charisma': 7, 'Confidence': 7}),
+                Card("Whoopi Goldberg", "placeholder.png", {'Acting': 8, 'Comedy': 9, 'Charisma': 8, 'Confidence': 10}),
+                Card("Meryl Streep", "placeholder.png", {'Acting': 10, 'Comedy': 6, 'Charisma': 9, 'Confidence': 8}),
             ],
             "Academic": [
                 Card("Ruth Bader Ginsburg", "ruth.jpg",
                      {'Intelligence': 9, 'Political Influence': 9, 'Determination': 6, 'Rebellious': 9}),
                 Card("Hypatia", "placeholder.png", {'Intelligence': 10, 'Political Influence': 3, 'Determination': 9, 'Rebellious': 10}),
-                Card("Rosa Parks", "rosa.jpg", {'Intelligence': 8, 'Courage': 10, 'Resilience': 9}),
+                Card("Rosa Parks", "rosa.jpg", {'Intelligence': 8, 'Courage': 10, 'Resilience': 9, 'Rebellious': 10}),
             ],
             "Activist": [
                 Card("Florence Given", "placeholder.png", {'Confidence': 10, 'Activism': 10, 'Empowerment': 9, 'Social Influence': 8}),
