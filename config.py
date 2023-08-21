c4e9df042f28e1835249caf99c119eaba2d24c40
Adaeze Eke <adaeze_eke@outlook.com>
2023-08-21 18:00:49 +0100

    Set this back to default

--- config.py
+++ config.py
@@ -1,26 +1,6 @@
-import mysql.connector
-
-
 db_config = {
-    'host': '127.0.0.1',
+    'host': 'localhost',
     'user': 'root',
-    'password': 'Omoyemhe.1',
+    'password': '',
     'database': 'player_cards'
 }
-
-connection = mysql.connector.connect(**db_config)
-
-cursor = connection.cursor()
-
-# Example query to fetch data from the table
-query = "SELECT * FROM attributes"
-cursor.execute(query)
-attributes = cursor.fetchall()
-
-for attribute in attributes:
-    print(attribute)
-
-cursor.close()
-connection.close()
-
-# Added details to help with sql setup
