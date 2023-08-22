9821e190a4bc76cb924af2a399064f320eb53036
Emma Holden <emmaholden01204@gmail.com>
2023-08-22 20:13:44 +0100

    changed config file to be more user-friendly  and updated the readme to reflect the rest of the set up the user needs to do to run the file

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
 API_KEY = ""
+HOST = 'localhost'
+USER = 'root'
+PASSWORD = ""
 
 player_db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
+    'host': HOST,
+    'user': USER,
+    'password': PASSWORD,
     'database': 'player_cards'
 }
 
 challenge_db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
+    'host': HOST,
+    'user': USER,
+    'password': PASSWORD,
     'database': 'challenge_cards'
 }
 
