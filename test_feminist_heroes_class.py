c0d9b12372d18e74e1627e34783f12f23270dda2
Emma Holden <emmaholden91@yahoo.com>
2023-08-25 11:15:28 +0100

    added reset function tests

--- test_feminist_heroes_class.py
+++ test_feminist_heroes_class.py
@@ -96,6 +96,32 @@ class TestCardChoice(unittest.TestCase):
         self.assertEqual(chosen_card.name, "Card 2")
         self.assertEqual(len(self.player.cards), 1)
 
+class TestReset(unittest.TestCase):
+
+    def setUp(self):
+        self.game = FeministHeroesVsChallenges()
+
+    def test_function_initializes_game(self):
+        self.game.reset()
+        self.assertIsNotNone(self.game.player_deck)
+        self.assertIsNotNone(self.game.player)
+        self.assertIsNotNone(self.game.challenge_deck)
+
+    def test_function_resets_scores(self):
+        self.game.computer_score = 5
+        self.game.player_score = 3
+        self.game.reset()
+        self.assertEqual(self.game.player_score, 0)
+        self.assertEqual(self.game.computer_score, 0)
+
+    def test_function_resets_decks(self):
+        self.game.choose_card(0)
+        self.game.challenge_deck.draw()
+        challenge_deck_length_after_draw = len(self.game.challenge_deck.cards)
+        self.game.reset()
+        self.assertEqual(len(self.game.player_deck.generate_player_cards()), 4)
+        self.assertNotEquals(challenge_deck_length_after_draw, len(self.game.challenge_deck.cards))
+
 
 class MockFeministHeroesVsChallenges:
     def __init__(self):
